1f985088e16ef8f260eef71b9352f4ce593442fc
Ivar Eriksen <ivareri@stud.ntnu.no>
2016-12-16 17:10:58 +0100

    Fixed paths

--- gantry_support/scripts/blender_urdf_to_xacro.sh
+++ gantry_support/scripts/blender_urdf_to_xacro.sh
@@ -2,5 +2,12 @@
 # Converts URDF file from blender into wokring xacro macro file
 # Reads replacments from .sed files
 
-sed -f urdf_to_xacro.sed ../urdf/gantry.urdf > ../urdf/gantry_macro.xacro
-sed -f gantry_joint_names.sed -i ../urdf/gantry_macro.xacro
+gantry_support_dir=$(rospack find gantry_support)
+echo $gantry_support_dir
+
+# Do replacements
+sed -f $gantry_support_dir/scripts/urdf_to_xacro.sed $gantry_support_dir/urdf/gantry.urdf > $gantry_support_dir/urdf/gantry_macro.xacro
+sed -f $gantry_support_dir/scripts/gantry_joint_names.sed -i $gantry_support_dir/urdf/gantry_macro.xacro
+
+# Generate urdf from xacro files. Usefull for MoveIt! setup assistant
+$(rospack find xacro)/xacro.py --inorder $gantry_support_dir/urdf/gantry_robot.xacro > $gantry_support_dir/urdf/gantry_robot.urdf
